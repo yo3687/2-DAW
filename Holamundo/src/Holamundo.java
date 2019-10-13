0418682a4b815729fc70b78c6f5ae59a28710861
Jonathan <yonafp3687@gmail.com>
2019-10-13 20:50:37 +0200

    Commit

---
+++ Holamundo/src/Holamundo.java
@@ -0,0 +1,12 @@
+
+public class Holamundo {
+
+	public static void main(String[] args) {
+		System.out.println("hola mundo");
+		System.out.println("que tal");
+		System.out.println("Eu ben");
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Holamundo/src/coche.java
@@ -0,0 +1,10 @@
+
+public class coche {
+
+	public static void main(String[] args) {
+		System.out.println("son un coche");
+		// TODO Auto-generated method stub
+
+	}
+
+}
